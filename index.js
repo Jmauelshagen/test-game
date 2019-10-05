bbfa0a4fe9512370d1c93cab025dc68873f93fee
Jeffrey Mauelshagen <jbm3014@c02y40tvjg5mmbp.hsd1.ga.comcast.net>
2019-10-04 23:21:38 -0400

    added beginner pro and expert mode

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const browser = document.getElementById("main-container");
-const playButton = document.getElementById("play");
+const gameScreen = document.getElementById("main-container");
+const playBeginnerButton = document.getElementById("playB");
+const playProButton = document.getElementById("playP");
+const playExpertButton = document.getElementById("playE");
 const stopButton = document.getElementById("stop");
 const colors = ['red','orange','blue','green'];
 let whiteSquares = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 let playing = false;
+let count = 0;
 
 const timer = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-let play = async () => {
+let playExpert = async () => {
     playing = true;
     while(playing === true) {
-        changecolor();
-        await timer(500);
+        while (whiteSquares.length !== 0){
+            changecolor();
+            await timer(200);
+        }
+        stopGame();
+    }
+}
+let playPro = async () => {
+    playing = true;
+    while(playing === true) {
+        while (whiteSquares.length !== 0){
+            changecolor();
+            await timer(400);
+        }
+        stopGame();
+    }
+}
+let playBeginner = async () => {
+    playing = true;
+    while(playing === true) {
+        while (whiteSquares.length !== 0){
+            console.log(whiteSquares.length);
+            changecolor();
+            await timer(600);
+        }
+        stopGame();
     }
 }
 
@@ -28,23 +55,34 @@ const changecolor = () =>{
     }
 }
 
-const startGame = () => {
-    play();
-};
-
 const stopGame = () => {
     playing = false;
+    for (i = 1 ; i < 17 ; i++){
+        const item = document.getElementById(i);
+        item.classList.replace(item.classList[1], 'white');
+        let int = whiteSquares.findIndex((i) => i === item.id);
+        if (int === -1){
+            whiteSquares.push(item.id)
+        }
+    }
+    alert(`Game over! you got ${count} items.`);
+    count = 0;
 };
 
-const test = (e) => {
+const clickBox = (e) => {
     const item = e.target;
-    item.classList.replace(item.classList[1], 'white');
-    let int = whiteSquares.findIndex((i) => i === item.id);
-    if (int === -1){
-        whiteSquares.push(item.id)
+    if(item.classList[1] !== 'white'){
+        count++;
+        item.classList.replace(item.classList[1], 'white');
+        let int = whiteSquares.findIndex((i) => i === item.id);
+        if (int === -1){
+            whiteSquares.push(item.id)
+        }
     }
 };
 
-browser.addEventListener('click', test);
-playButton.addEventListener('click', startGame);
+gameScreen.addEventListener('click', clickBox);
+playBeginnerButton.addEventListener('click', playBeginner);
+playProButton.addEventListener('click', playPro);
+playExpertButton.addEventListener('click', playExpert);
 stopButton.addEventListener('click', stopGame);
